23529798feb83bb0db2b7bfdeff64d8767dcedcf
Iakov32 <152983924+Iakov32@users.noreply.github.com>
2024-01-08 17:55:05 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,2 @@
+URL_SERVICE =  "https://6cd63f66-fb09-48d9-8658-25c2229edf45.serverhub.praktikum-services.ru"
+CREAT_ORDERS = "/api/v1/orders"
---
+++ data.py
@@ -0,0 +1,11 @@
+order_body = {
+        "firstName": "Iakov",
+        "lastName": "Balabko",
+        "address": "Komarova, 51",
+        "metroStation": 4,
+        "phone": "+7 920 854 04 69",
+        "rentTime": 5,
+        "deliveryDate": "2024-01-09",
+        "comment": "Hi",
+        "color": ["BLACK"]
+    }
